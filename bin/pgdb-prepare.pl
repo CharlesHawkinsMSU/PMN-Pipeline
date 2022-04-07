9c2a6e073ad1c475a2015bb5bb8fb43158fe09d3
Charles Hawkins <chawkins@carnegiescience.edu>
2022-04-06 18:57:27 -0700

    New pgdb-table.txt format; perl function to read the file; pgdb-prepare now uses it; new script to set up environment variables

--- bin/pgdb-prepare.pl
+++ bin/pgdb-prepare.pl
@@ -2,7 +2,7 @@
 
 # Chuan Wang @ Rhee-lab, Department of Plant Biology, Carnegie Institution for Science
 # Date: Jul., 2014
-# Last updated by Charles Hawkins, Apr 2021
+# Last updated by Charles Hawkins, Apr 2022
 
 # Description: Prepare data file folders for generating PGDBs by ptools
 # Input: table of basic info, pf file folder, prepared folder, ptools executable full path
@@ -10,7 +10,7 @@
 
 my $usage = <<USAGE;
 
-  Usage: $0 <input-table-file> <pf-file-folder> <output-folder> <ptools-full-path> <pgdb-full-path>
+  Usage: $0 <input-table-file> <pf-file-folder> <output-folder> <ptools-full-path>
 
 USAGE
 
@@ -18,86 +18,95 @@ use strict;
 use Cwd;
 use File::Spec;
 use File::Basename;
+use lib dirname(__FILE__);
+use pmn;
 
 # Check arguments
 if ($#ARGV<0) {
 	die $usage;
 }
 
-$|=1;   # Output prior
-my $path = dirname(File::Spec->rel2abs(__FILE__));  # Path of this script
-my $cmdline = "$0 ".join(' ',@ARGV);
+# Extract out arguments into variables
+my $table_file = $ARGV[0];
+my $pf_dir = $ARGV[1];
+my $masters_dir = $ARGV[2];
+my $ptools = $ARGV[3];
 
-$ARGV[1] =~ s!/$!!;
-$ARGV[2] =~ s!/$!!;
-mkdir $ARGV[2];
+$|=1;   # Output prior
+
+
+$pf_dir =~ s!/$!!;
+$masters_dir =~ s!/$!!;
+mkdir $masters_dir;
 
 # read input table file
-my %data;
-open(IN, "<$ARGV[0]") or die "Cannot open file $ARGV[0]!\n";
+my %pgdb_table = pmn::read_pgdb_table($table_file);
 
-while (<IN>) {
-    chomp;
-    my @r = split /\t/;
-    $data{$r[0]} = \@r;
-}
+# for each species
 
-close IN;
-
-# for each specie
-my @sp = keys %data;
-
-foreach my $s (@sp) {
-    next if $s =~ /^Database/;
+foreach my $species (keys %pgdb_table) {
     
-    # create folder for species $s
-    mkdir "$ARGV[2]/$s";
+	# Extract the entry data into variables
+	my $entry = $pgdb_table{$species};
+	my $pf_file = $$entry{"PF File"};
+	my $org_counter = counter_from_id($$entry{"Unique ID"});
+	my $id_name = $$entry{"ID/Name"};
+	my $sp_name = $$entry{"Species Name"};
+	my $db_name = $$entry{"Database Name"};
+	my $ab_name = $$entry{"Abbrev Name"};
+	my $seq_file = $$entry{"Sequence File"};
+	my $taxid = $$entry{"NCBI Taxon ID"};
+	my $pgdb_version = $$entry{"Version"};
+	my $homepage = $$entry{"Homepage"};
+	my $email = $$entry{"Email"};
+
+    # create folder for species $species
+    mkdir "$masters_dir/$species";
     
     # copy pf file to the folder
-    `cp $ARGV[1]/$data{$s}[7] $ARGV[2]/$s/`;
-    $data{$s}[8] = convert($data{$s}[8]);
-    print STDERR "Preparing for $s (PGDB-UNIQUE-ID: $data{$s}[8])...\n";
+    `cp $pf_dir/$pf_file $masters_dir/$species/$pf_file`;
+    print STDERR "Preparing for $species (PGDB-UNIQUE-ID: $org_counter)...\n";
 
     # write create.master file
-    open(my $cf, '>', "$ARGV[2]/$s/create.master") or die $!;
-    print $cf $ARGV[3];
+    open(my $cf, '>', "$masters_dir/$species/create.master") or die $!;
+    print $cf $ptools;
     close $cf;
 
     # write genetic-elements.dat file
-    open(GE, ">$ARGV[2]/$s/genetic-elements.dat") or die "Cannot open file $ARGV[2]/$s/genetic-elements.dat to write.\n";
-    print GE "ID\t$data{$s}[3]\nNAME\t$data{$s}[3]\nANNOT-FILE\t$data{$s}[7]\n//\n";
+    open(GE, ">$masters_dir/$species/genetic-elements.dat") or die "Cannot open file $masters_dir/$species/genetic-elements.dat to write.\n";
+    print GE "ID\t$id_name\nNAME\t$id_name\nANNOT-FILE\t$pf_file\n//\n";
     close GE;
     
     # write organism-params.dat file
-    open(OP, ">$ARGV[2]/$s/organism-params.dat") or die "Cannot open file $ARGV[2]/$s/organism-params.dat to write.\n";
-    print OP "ID\t$s\nSTORAGE\tFILE\nNAME\t$data{$s}[2]\nABBREV-NAME\t$data{$s}[4]\nSEQUENCE-SOURCE\t$data{$s}[6]\nDOMAIN\t$data{$s}[5]\nRANK\t|species|\nCODON-TABLE\t1\nMITO-CODON-TABLE\t1\nHOMEPAGE\twww.plantcyc.org\nEMAIL\tcurator\@plantcyc.org\nDBNAME\t$data{$s}[1]\nNCBI-TAXON-ID\t$data{$s}[5]\nREF-ORGID\tPLANT\nORG-COUNTER\t$data{$s}[8]\n";
+    open(OP, ">$masters_dir/$species/organism-params.dat") or die "Cannot open file $masters_dir/$species/organism-params.dat to write.\n";
+    print OP "ID\t$species\nSTORAGE\tFILE\nNAME\t$sp_name\nABBREV-NAME\t$ab_name\nSEQUENCE-SOURCE\t$seq_file\nDOMAIN\t$taxid\nRANK\t|species|\nCODON-TABLE\t1\nMITO-CODON-TABLE\t1\nHOMEPAGE\t$homepage\nEMAIL\t$email\nDBNAME\t$db_name\nNCBI-TAXON-ID\t$taxid\nREF-ORGID\tPLANT\nORG-COUNTER\t$org_counter\n";
     close OP;
      
     # write dump.master foreach species
-    open(DP, ">$ARGV[2]/$s/dump.master") or die "Cannot open file $ARGV[2]/$s/dump.master to write.\n";
-    print DP "s_ptools\t$ARGV[3]\nlisp\t(so '$s)\nlisp\t(setq *file-export-progress* nil)\nlisp\t(dump-frames-to-attribute-value-files (org-data-dir))\nlisp\t(dump-frames-to-tabular-files (org-data-dir))\n";
+    open(DP, ">$masters_dir/$species/dump.master") or die "Cannot open file $masters_dir/$species/dump.master to write.\n";
+    print DP "s_ptools\t$ptools\nlisp\t(so '$species)\nlisp\t(setq *file-export-progress* nil)\nlisp\t(dump-frames-to-attribute-value-files (org-data-dir))\nlisp\t(dump-frames-to-tabular-files (org-data-dir))\n";
     close DP;
     
     # write dump-biopax.master foreach species
-    open(DP, ">$ARGV[2]/$s/dump-biopax.master") or die "Cannot open file $ARGV[2]/$s/dump-biopax.master to write.\n";
-    print DP "s_ptools\t$ARGV[3]\nlisp\t(so '$s)\nlisp\t(setq *file-export-progress* nil)\nlisp\t(com-export-pgdb-to-biopax)";
+    open(DP, ">$masters_dir/$species/dump-biopax.master") or die "Cannot open file $masters_dir/$species/dump-biopax.master to write.\n";
+    print DP "s_ptools\t$ptools\nlisp\t(so '$species)\nlisp\t(setq *file-export-progress* nil)\nlisp\t(com-export-pgdb-to-biopax)";
     close DP;
     
     # write checker.master foreach species
-    open(CH, ">$ARGV[2]/$s/checker.master") or die "Cannot open file $ARGV[2]/$s/checker.master to write.\n";
-    print CH "s_ptools\t$ARGV[3]\nlisp\t(so '$s)\nlisp\t(run-all-checks)\nlisp\t(save-kb)\n";
+    open(CH, ">$masters_dir/$species/checker.master") or die "Cannot open file $masters_dir/$species/checker.master to write.\n";
+    print CH "s_ptools\t$ptools\nlisp\t(so '$species)\nlisp\t(run-all-checks)\nlisp\t(save-kb)\n";
     close CH;
     
     # write newversion.master foreach species
-    if ($data{$s}[9]) {
-	open(NV, ">$ARGV[2]/$s/newversion.master") or die "Cannot open file $ARGV[2]/$s/newversion.master to write.\n";
-	print NV "s_ptools\t$ARGV[3]\nlisp\t(so '$s)\nlisp\t(create-new-pgdb-version (current-kb) \"$data{$s}[9]\")\n";
+    if ($pgdb_version) {
+	open(NV, ">$masters_dir/$species/newversion.master") or die "Cannot open file $masters_dir/$species/newversion.master to write.\n";
+	print NV "s_ptools\t$ptools\nlisp\t(so '$species)\nlisp\t(create-new-pgdb-version (current-kb) \"$pgdb_version\")\n";
 	close NV;
     }
 }
 
 #######
-sub convert {
+sub counter_from_id {
     my $l = shift;
     my @d = split //, $l;
     my $sum = 0;
---
+++ bin/pmn.pm
@@ -0,0 +1,165 @@
+# Charles Hawkins at Rhee Lab, Department of Plant Biology, Carnegie
+# Institution for Science
+#
+# Created: April 2022
+#
+# Description: This perl module defines the function to read the pgdb-table.txt
+# (or to-create-PGDBs.txt as it was formerly known) file and put it all into a
+# dictionary mapping from orgids (e.g. "ara" for Aracyc) to a dictionary of the
+# fields in the file. The header line is required and defines the order of the
+# columns. Some columns are computed if they are missing or blank for a given
+# organism.
+
+package pmn;
+$VERSION="0.1";
+use strict;
+
+# @individual_required_fields are required for each organism, and cannot be
+# specified using /default
+my @individual_required_fields;
+@individual_required_fields = ("Database ID", "Species Name", "NCBI Taxon ID", "Sequence File", "Unique ID", "Version", "Seq Source");
+# @required_fields are required, but can be specified using /default
+my @required_fields;
+@required_fields = ("Authors", "Curator", "Citation Year");
+my @all_required_fields;
+@all_required_fields = (@individual_required_fields, @required_fields);
+sub read_pgdb_table
+{
+	my $table_filename = shift;
+	my %pgdb_table;
+	my %req_fields_empty;   # Keys are @required_fields; value is true if at least one db has no value for that field
+	my %req_fields_default; # Keys are @required_fields; value is true if /default has a value for that field
+	open(TABLE, $table_filename) or die "$table_filename: $!\n";
+
+	# We will first read in the whole file and manually split it into lines so
+	# that we can be independent of DOS/Unix/Mac newlines (who knows what kind
+	# of newlines the user's spreadsheet program will put in). Also causes us
+	# to ignore empty lines
+	my $table_file;
+	local $/=undef; $table_file=<TABLE>;
+	my @table_lines = split /[\r\n]+/, $table_file;
+
+	# The first line is the header. We will go through it, check that required
+	# fields are present, and then build a map from header fields to column
+	# numbers so we know what column is what later
+	my $header = $table_lines[0];
+	chomp $header;
+	my @hfields = split(/\t/, $header);
+	# Remove quote marks from around all the fields (spreadsheets like to put
+	# quotes around the cells)
+	foreach my $f (@hfields)
+	{
+		$f =~ s/\A"(.*)"\Z/\1/;
+	}
+
+	# Check that all required fields are present
+	my %hf_hash = map {$_ => 1} @hfields;
+	foreach my $f (@all_required_fields)
+	{
+		if(!exists($hf_hash{$f}))
+		{
+			die "Field is required in $table_filename: $f\n"
+		}
+	}
+
+	# Get the index of each named column
+	my %field_index;  # Maps from column names to column numbers
+	for my $hi (0 .. $#hfields)
+	{
+		$field_index{$hfields[$hi]} = $hi;
+	}
+
+	# Now iterate through the table
+	my $line_n = 2;	# Line number, used purely for error messages
+	foreach $_ (@table_lines[1..$#table_lines])
+	{
+		my @fields = split(/\t/);
+		# Remove quotes, as in the header
+		foreach my $f (@fields)
+		{
+			$f =~ s/\A"(.*)"\Z/\1/;
+		}
+
+		# The Database ID field is what we'll be indexing by
+		my $db_id = $fields[$field_index{"Database ID"}];
+		if ($db_id ne "/default")
+		{
+			# Check that this entry has all individually-required fields
+			foreach my $f (@individual_required_fields)
+			{
+				if(!$fields[$f])
+				{
+					die "Line $line_n in $table_filename is missing required field: $f\n"
+				}
+			}
+		}
+		my %entry;  # The hash for this species entry; maps from column names to the value in that column for this species
+		foreach my $field (keys %field_index)
+		{
+			$entry{$field} = $fields[$field_index{$field}];
+		}
+		$pgdb_table{$db_id} = \%entry;
+		$line_n ++;
+	}
+
+	# Now go through our table and fill in default values from the /default entry
+	my $default_entry = $pgdb_table{"/default"};
+	foreach my $db_id (keys %pgdb_table)
+	{
+		my $entry = $pgdb_table{$db_id};
+		foreach my $field (keys %field_index)
+		{
+			if (!$$entry{$field})
+			{
+				$$entry{$field} = $$default_entry{$field};	# Will remain empty if default_entry doesn't exist or is empty for $field
+			}
+		}
+	}
+
+	# Now fill in computed values
+	foreach my $db_id (keys %pgdb_table)
+	{
+		my $db_entry = $pgdb_table{$db_id};
+
+		$$db_entry{"Database Name"} ||= $$db_entry{"Database ID"}."Cyc";
+		my @species_words = split(/ /, $$db_entry{"Species Name"});
+		$$db_entry{"ID/Name"} ||= substr($species_words[0], 0, 1).$species_words[1];
+		if (!$$db_entry{"Abbrev Name"})
+		{
+			# The "Abbrev Name" field is, e.g., "A. thaliana col". Unlike
+			# "ID/Name" it gets subspecies included but shouldn't have things
+			# like "var.", so e.g. Brassica oleracea var. capitata would
+			# translate to "B. oleracea capitata". As a short cut, we just
+			# remove any words after the species name that end in a ".". We
+			# don't apply this to the species name itself (i.e. the first word
+			# after the genus) because of things like "Xenia sp."
+			my $species_and_subsp = $species_words[1];
+			for my $i (2 .. $#species_words)
+			{
+				if ($species_words[$i] !~ /\.\z/)
+				{
+					$species_and_subsp .= " ".$species_words[$i];
+				}
+			}
+
+			$$db_entry{"Abbrev Name"} = substr($species_words[0], 0, 1).". ".$species_and_subsp;
+		}
+		$$db_entry{"Initial PF File"} ||= $$db_entry{"Sequence File"}.".e2p2v4.orxn.pf";
+		if (!$$db_entry{"PF File"})
+		{
+			my $pf_file = $$db_entry{"Initial PF File"};
+			$pf_file =~ s/(\.pf)?\z/.revised$&/;
+			$$db_entry{"PF File"} = $pf_file;
+		}
+		my $year = $$db_entry{"Citation Year"};
+		$$db_entry{"SAVI Citation"} ||= "|PUB-PMNUPP$year| |PUB-PMNAIPP$year| |PUB-PMNRXN$year| |PUB-PMNRXNTAXON$year| |PUB-PMNTAXON$year| |PUB-PMNIC$year| |PUB-PMNSP$year|";
+		$$db_entry{"E2P2 Citation"} ||= "|PUB-E2P2PMN$year|";
+		$$db_entry{"Enzrxn Citation"} ||= "E2P2PMN$year:EV-COMP-AINF";
+		$$db_entry{"ENZ name file"} ||= "ec_name.map.parsed";
+		$$db_entry{"RXN Map"} ||= "metacyc-rxn-name-mapping";
+		$$db_entry{"PWY Metacyc"} ||= "all_pwy.meta";
+		$$db_entry{"PWY Plantcyc"} ||= "all_pwy.plant";
+	}
+	delete $pgdb_table{"/default"};
+	return %pgdb_table;
+}
